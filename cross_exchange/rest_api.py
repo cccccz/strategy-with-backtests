d72a0ec21e39c971959363b65ce25225ba0f4119
cccccz <chenzheng0430@gmail.com>
2025-08-05 15:37:55 +0800

    working

---
+++ cross_exchange/rest_api.py
@@ -0,0 +1,152 @@
+import requests
+import pandas as pd
+from functools import reduce
+
+def fetch_all_symbols(exchange, market_type="spot"):
+    """获取交易所支持的所有交易对（现货或合约）"""
+    suffix = None
+
+    if exchange == "binance":
+        if market_type == 'spot':
+            url = "https://api.binance.com/api/v3/exchangeInfo"
+            data = requests.get(url).json()
+            symbols = [s["symbol"] for s in data["symbols"] if s["status"] == "TRADING"]
+        elif market_type == "futures_p":
+            url = "https://fapi.binance.com/fapi/v1/exchangeInfo"
+            data = requests.get(url).json()
+            symbols = [s["symbol"] for s in data["symbols"] if s["status"] == "TRADING"]
+        elif market_type == "futures_d":
+            url = "https://dapi.binance.com/dapi/v1/exchangeInfo"
+            data = requests.get(url).json()
+            symbols = [s["symbol"] for s in data["symbols"] if s["contractStatus"] == "TRADING"]
+
+    
+    elif exchange == "bybit":
+        if market_type == 'spot':
+            suffix = 'spot'
+        elif market_type == 'futures_p':
+            suffix = 'linear'
+        elif market_type == 'futures_d':
+            suffix = 'inverse'
+            
+        url = "https://api.bybit.com/v5/market/instruments-info?category=" + suffix
+        data = requests.get(url).json()
+        symbols = [s["symbol"] for s in data["result"]["list"] if s["status"] == "Trading"]
+    
+    elif exchange == "okx":
+        if market_type == 'spot':
+            suffix = 'SPOT'
+        elif market_type == 'futures_p':
+            suffix = 'SWAP'
+        elif market_type == 'futures_d':
+            suffix = 'FUTURES'
+        url = "https://www.okx.com/api/v5/public/instruments?instType=" + suffix
+        data = requests.get(url).json()
+        symbols = [s["instId"] for s in data["data"]]
+
+    
+    elif exchange == "bitget":
+        if market_type == 'spot':
+            url = "https://api.bitget.com/api/spot/v1/public/products"
+        
+        elif market_type == 'futures_p':
+            url = 'https://api.bitget.com/api/v2/mix/market/contracts?productType=usdt-futures'
+        elif market_type == 'futures_d':
+            url = "https://api.bitget.com/api/v2/mix/market/contracts?productType=coin-futures"
+        data = requests.get(url).json()
+        symbols = [s["symbol"] for s in data["data"]]
+    
+    else:
+        raise ValueError("Unsupported exchange")
+    
+    return symbols
+
+def get_all_symbols():
+    platforms = ['binance', 'okx', 'bitget', 'bybit']
+    market_types = ['spot', 'futures_p', 'futures_d']
+    all_data = {}
+
+    for platform in platforms:
+        all_data[platform] = {}
+        for market_type in market_types:
+            try:
+                symbols = fetch_all_symbols(platform, market_type)  # Now uses market_type
+                all_data[platform][market_type] = symbols
+                print(f"[SUCCESS] {platform} {market_type}: {len(symbols)} symbols")
+            except Exception as e:
+                print(f"[ERROR] {platform} {market_type}: {str(e)}")
+                all_data[platform][market_type] = []  # Store empty list if failed
+
+    return all_data
+
+def save_symbols(all_data,file_path="./cross_exchange/exchange_symbols.csv"):
+        # Convert all_data to a DataFrame
+    rows = []
+    for exchange in all_data:
+        for market_type in all_data[exchange]:
+            symbols = ",".join(all_data[exchange][market_type])  # Join symbols with commas
+            rows.append({
+                "Exchange": exchange,
+                "Market Type": market_type,
+                "Symbols": symbols
+            })
+
+    df = pd.DataFrame(rows)
+
+    # Save to CSV
+    df.to_csv(file_path, index=False)
+    print(f'Saved to "{file_path}')
+
+def get_all_symbols_and_save():
+    all_data = get_all_symbols()
+    save_symbols(all_data)
+
+def clean_and_save():
+    '''symbols get removed if does not contain USDT'''
+    df = pd.read_csv('./cross_exchange/exchange_symbols.csv')
+    df = df[df['Market Type'] != 'futures_d']
+    df = df[df['Market Type'] != 'spot']
+
+
+    filtered_symbols = []
+    
+    for symbols_str in df['Symbols']:
+        symbols_list = symbols_str.split(',')
+        usdt_symbols = []
+        for symbol in symbols_list:
+            if 'usdt' in symbol.lower():
+                usdt_symbols.append(symbol)
+        
+        filtered_str = ','.join(usdt_symbols) if usdt_symbols else ''
+        filtered_symbols.append(filtered_str)
+    
+    df['Symbols'] = filtered_symbols
+    df = df[df['Symbols'] != ''].reset_index(drop=True)
+    print(df.head())
+    file_path = './cross_exchange/cleaned_symbols.csv'
+    df.to_csv(file_path, index=False)
+    print(f'Saved to "{file_path}')
+
+def format_symbols():
+    '''remove '-', '-SWAP' from symbols'''
+    df = pd.read_csv('./cross_exchange/cleaned_symbols.csv')
+    df['Symbols'] = df['Symbols'].str.replace('-SWAP','')
+    df['Symbols'] = df['Symbols'].str.replace('-','')
+    df = df[df['Symbols'] != ''].reset_index(drop=True)
+    print(df.head())
+    file_path = './cross_exchange/cleaner_symbols.csv'
+    df.to_csv(file_path, index=False)
+    print(f'Saved to "{file_path}')
+
+def get_common_symbols():
+    name_lists = []
+    df = pd.read_csv('./cross_exchange/cleaner_symbols.csv')
+    for symbols_str in df['Symbols']:
+        symbols_list = symbols_str.split(',')
+        name_lists.append(symbols_list)
+    common_symbols = set(reduce(lambda x, y:set(x) & set(y), name_lists))
+    return list(common_symbols)
+
+if __name__ == '__main__':
+
+    print("hello world!")
---
+++ cross_exchange/websockets_api.py
@@ -0,0 +1,132 @@
+import asyncio
+import json
+import websockets
+import os
+import time
+from rest_api import get_common_symbols
+
+exchanges = ['Binance', 'OKX', 'Bitget', 'Bybit']
+symbols = get_common_symbols()
+#print(f"monitoring {len(symbols)} currencies") 163
+shared_data = {
+    symbol:{exchange:{"bid":None,"ask":None}for exchange in exchanges}for symbol in symbols
+}
+
+# -------- Terminal 清屏函数 --------
+def clear_terminal():
+    os.system("cls" if os.name == "nt" else "clear")
+
+async def display_terminal():
+    while True:
+        clear_terminal()
+        print("+------------+-----------+-------------+-------------+")
+        print("| Exchange   | Symbol    | Bid Price   | Ask Price   |")
+        print("+------------+-----------+-------------+-------------+")
+
+        for exchange, symbols in shared_data.items():
+            for symbol, data in symbols.items():
+                bid = data['bid'] if data['bid'] else "..."
+                ask = data['ask'] if data['ask'] else "..."
+                print(f"| {exchange:<10} | {symbol:<9} | {str(bid):<11} | {str(ask):<11} |")
+
+        print("+------------+-----------+-------------+-------------+")
+        print(f"Last update: {time.strftime('%Y-%m-%d %H:%M:%S')}")
+        await asyncio.sleep(1)
+
+
+# -------- Binance --------
+async def binance_ws():
+    uri = f"wss://stream.binance.com:9443/ws/btcusdt@bookTicker"
+    async with websockets.connect(uri) as ws:
+        while True:
+            msg = await ws.recv()
+            data = json.loads(msg)
+            bid = data['b']
+            ask = data['a']
+            shared_data["Binance"]["BTCUSDT"]["bid"] = bid
+            shared_data["Binance"]["BTCUSDT"]["ask"] = ask
+
+            # print(f"[Binance] BTCUSDT: bid={bid} ask={ask}")
+
+# -------- Bybit --------
+async def bybit_ws():
+    uri = "wss://stream.bybit.com/v5/public/spot"
+    async with websockets.connect(uri) as ws:
+        subscribe_msg = {
+            "op": "subscribe",
+            "args": ["orderbook.1.BTCUSDT"]
+        }
+        await ws.send(json.dumps(subscribe_msg))
+
+        while True:
+            msg = await ws.recv()
+            data = json.loads(msg)
+            if 'data' in data:
+                bid = data['data']['b'][0][0]
+                ask = data['data']['a'][0][0]
+                shared_data["Bybit"]["BTCUSDT"]["bid"] = bid
+                shared_data["Bybit"]["BTCUSDT"]["ask"] = ask
+                # print(f"[Bybit]   BTCUSDT: bid={bid} ask={ask}")
+
+# -------- Bitget --------
+async def bitget_ws():
+    uri = "wss://ws.bitget.com/v2/ws/public"
+    async with websockets.connect(uri) as ws:
+        subscribe_msg = {
+            "op": "subscribe",
+            "args": [{
+                "instType": "SPOT",
+                "channel": "ticker",
+                "instId": "BTCUSDT"
+            }]
+        }
+        await ws.send(json.dumps(subscribe_msg))
+
+        while True:
+            msg = await ws.recv()
+            data = json.loads(msg)
+            if 'data' in data and len(data['data']) > 0:
+                item = data['data'][0]
+                bid = item['bidPr']
+                ask = item['askPr']
+                shared_data["Bitget"]["BTCUSDT"]["bid"] = bid
+                shared_data["Bitget"]["BTCUSDT"]["ask"] = ask
+                # print(f"[Bitget]  BTCUSDT: bid={bid} ask={ask}")
+
+# -------- OKX --------
+async def okx_ws():
+    uri = "wss://ws.okx.com:8443/ws/v5/public"
+    async with websockets.connect(uri) as ws:
+        subscribe_msg = {
+            "op": "subscribe",
+            "args": [{
+                "channel": "tickers",
+                "instId": "BTC-USDT-SWAP"
+            }]
+        }
+        await ws.send(json.dumps(subscribe_msg))
+
+        while True:
+            msg = await ws.recv()
+            data = json.loads(msg)
+            if 'data' in data and len(data['data']) > 0:
+                item = data['data'][0]
+                bid = item['bidPx']
+                ask = item['askPx']
+                shared_data["OKX"]["BTC-USDT-SWAP"]["bid"] = bid
+                shared_data["OKX"]["BTC-USDT-SWAP"]["ask"] = ask
+                # print(f"[OKX]     BTC-USDT: bid={bid} ask={ask}")
+
+# -------- 主程序，聚合运行 --------
+async def main():
+    await asyncio.gather(
+        binance_ws(),
+        bybit_ws(),
+        bitget_ws(),
+        okx_ws(),
+        display_terminal()
+    )
+
+if __name__ == '__main__':  
+# 启动异步主程序
+    asyncio.run(main())
